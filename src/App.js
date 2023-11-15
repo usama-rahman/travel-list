41537cdaccb1348d857a06da688c3dc69281c7e6
Usama Rahman <rahmanusama390@gmail.com>
2023-11-15 11:16:38 +0600

    Travel

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const initialItems = [
+  { id: 1, description: " Passports", quantity: 2, packed: false },
+  { id: 2, description: " Socks", quantity: 12, packed: false },
+  { id: 3, description: " Pants", quantity: 3, packed: true },
+];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1> 🌴 Travels 👜 </h1>;
+}
+
+function Form() {
+  function handelSubmit(e) {
+    e.preventDefault();
+    console.log(e);
+  }
+  return (
+    <form className="add-form" onSubmit={handelSubmit}>
+      <h3>Packing list😍</h3>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item ..." />
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity}
+        {item.description}
+      </span>
+      <button> ❌ </button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em> your Bag Details </em>
+    </footer>
+  );
+}
